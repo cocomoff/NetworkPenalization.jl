0b051445b233d63272d49e6ed7e0468c7082600e
cocomoff <keisuke.otaki.jp@gmail.com>
2023-05-23 14:02:24 +0900

    update sample code with visualization

--- sample/example.jl
+++ sample/example.jl
@@ -1,8 +1,72 @@
 using Graphs
 using Random
 using NetworkPenalization
+using Measures
+using Colors
+using Plots
+gr()
 
-function main(;city="nihonbashi", K=5)
+
+function visualize(g, locations, pathset, dictC=weights(g))
+
+    xmin = minimum(minimum(locations[path, 1]) for path in pathset)
+    xmax = maximum(maximum(locations[path, 1]) for path in pathset)
+    ymin = minimum(minimum(locations[path, 2]) for path in pathset)
+    ymax = maximum(maximum(locations[path, 2]) for path in pathset)
+    delta = 1e-5
+    xlim = (xmin - delta, xmax + delta)
+    ylim = (ymin - delta, ymax + delta)
+
+
+    f = plot(figsize=(600, 600), xlims=xlim, ylims=ylim)
+    
+    # edges
+    for n in vertices(g)
+        for m in neighbors(g, n)
+            (n > m) && continue
+            pX = locations[[n, m], 1]
+            pY = locations[[n, m], 2]
+            plot!(f, pX, pY, linewidth=1, color=:black, label=nothing)
+        end
+    end
+
+    # nodes
+    scatter!(f, locations[:, 1], locations[:, 2], color=:tomato, label=nothing)
+
+    # paths
+    colors = distinguishable_colors(length(pathset))
+    for (ip, path) in enumerate(pathset)
+        for i in 1:num_edge(path)
+            pX = locations[[path[i], path[i + 1]], 1]
+            pY = locations[[path[i], path[i + 1]], 2]
+            plot!(f, pX, pY, linewidth=1, color=colors[ip], lw=5, alpha=0.5, label=nothing)
+        end
+    end
+
+    savefig(f, "output.png")
+
+
+    # metric trace plot
+    pK = 1:length(pathset)
+    pY1, pY2, pY3 = [], [], []
+    for k in pK
+        rK = pathset[1:k]
+        eval_ent = edge_load_entropy(rK)
+        eval_red = redundancy(g, rK, dictC)
+        eval_div = diversity(g, rK, dictC)
+        push!(pY1, eval_ent)
+        push!(pY2, eval_red)
+        push!(pY3, eval_div)
+    end
+
+    f = plot(size=(600, 300))
+    plot!(f, pK, pY1, marker=:v, label="ELE")
+    plot!(f, pK, pY2, marker=:o, label="Redundancy")
+    plot!(f, pK, pY3, marker=:^, label="Div")
+    savefig(f, "output-metric-trace.png")
+end
+
+function main(;city="nihonbashi", K=30)
     fn_loc = "$city-loc.txt"
     fn_road = "$city-road.txt"
 
@@ -65,7 +129,9 @@ function main(;city="nihonbashi", K=5)
     # penalization and K routes
     res = path_penalization(g, s, t, K, 0.5, dictC)
     for path in res
-        println(path)
+        dp = shortest_path_distance(g, path, dictC)
+        dp = round(dp, digits=3)
+        println(dp, " ", path[1:5])
     end
 
     # evaluate
@@ -76,6 +142,9 @@ function main(;city="nihonbashi", K=5)
     println("ELE(res):=$(eval_ent)")
     println("Red(res):=$(eval_red)")
     println("Div(res):=$(eval_div)")
+
+    # visualize
+    visualize(g, locations, res, dictC)
 end
 
 main()
